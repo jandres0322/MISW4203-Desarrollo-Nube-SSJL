e3f7e67dbcb19e3033398f279a156310ad2dddd6
Jhon Andres Parra Rodriguez <ja.parrar12@uniandes.edu.co>
2023-10-11 09:07:18 -0500

    creacion endpoints auth

---
+++ api/app.py
@@ -0,0 +1,138 @@
+from flask import Flask, request, jsonify, make_response
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import User, db
+from functools import wraps
+import jwt
+import datetime
+import os
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+app.config['JWT_TIME_EXPIRE'] = int(os.environ.get('JWT_TIME_EXPIRE',1200))
+app.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET_KEY", "JF}]&p1CH4-?-k]")
+
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        if 'authorization' in request.headers:
+            token = request.headers['authorization']
+            token = token.split(" ")[1]
+        if not token:
+            return jsonify({'message' : 'El token no encontrado'}), 401
+        print(token)
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], ["HS256"])
+            current_user = User.query\
+                .filter_by(username = data['username'])\
+                .first()
+        except Exception as e:
+            print(e)
+            return jsonify({
+                'message' : 'El token es invalido'
+            }), 401
+        return  f(current_user, *args, **kwargs)
+  
+    return decorated
+
+@app.route('/api/auth/login', methods=['POST'])
+def login_user():
+    auth = request.get_json()
+    if not auth or not auth.get('username') or not auth.get('password'):
+        return make_response(
+            jsonify({
+                'message': 'No hay información correspondiente'
+            }),
+            401
+        )
+    user = User.query.filter_by(username = auth.get('username')).first()
+    if not user:
+        return make_response(
+            jsonify({
+                'messaage': 'El usuario {} no se encuentra registrado'.format(auth.get('username')),
+            }),
+            401
+        )
+    if not check_password_hash(user.password, auth.get('password')):
+        return make_response(
+            jsonify({
+                'message': 'La contraseña no coincide',
+            }),
+            401
+        )
+    payload = {
+        'username': auth.get('username'),
+        'password': auth.get('password'),
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=app.config['JWT_TIME_EXPIRE'])
+    }
+    token = jwt.encode(payload, app.config['JWT_SECRET_KEY'],algorithm="HS256")
+    return make_response(
+        jsonify({
+            "token": token
+        }),
+        200
+    )
+    
+@app.route('/api/auth/signup', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    email = data.get('email')
+    username = data.get('username')
+    password1 = data.get('password1')
+    password2 = data.get('password2')
+    if password1 != password2:
+        return make_response(
+            jsonify({
+                'message': 'Las contraseñas no coinciden'
+            }),
+            400
+        )
+    user = User.query.filter_by(email = email).first()
+    if user:
+        return make_response(
+            jsonify({
+                'messsage': 'Ya existe un usuario con el correo {}'.format(email)
+            }),
+            400
+        )
+    user = User(
+        username = username,
+        password = generate_password_hash(password1),
+        email = email
+    )
+    db.session.add(user)
+    db.session.commit()
+    return make_response(
+        jsonify({
+            'message': 'Usuario creado exitosamente!',
+        }),
+        201
+    )
+
+@app.route('/api/example', methods=['POST'])
+@token_required
+def protected_route_example():
+    print("==== PROTECTED ROUTE EXAMPLE ==== ")
+    return make_response(
+        jsonify({
+            "message": "Acceso a ruta protegida"
+        }),
+        200
+    )
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000))
+    )
