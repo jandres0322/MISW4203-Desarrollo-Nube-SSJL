b0f09599ca44d520176f424e1b7afdae4caeeac6
Liceth Yaneth <l.rozoq@uniandes.edu.co>
2023-10-22 16:01:53 -0500

    Fix task celery

--- api/workers/schedules/tasks/video_task.py
+++ api/workers/schedules/tasks/video_task.py
@@ -1,10 +1,29 @@
 from celery import shared_task
-from api.services.process import read_messages_queue
+from models import Task
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 @shared_task(name='sync_sqs_logs', bind=True, max_retries=2)
 def sync_sqs_logs(self):
+    
     try:
-        read_messages_queue(self)
+        engine = create_engine('postgresql://root:password@database:5432/convert_tool_video')
+
+        # Crea el constructor de sesiones
+        Session = sessionmaker(bind=engine)
+
+        # Crea una sesión   
+        session = Session()
+    
+        # Realiza la consulta
+        tasks = session.query(Task).filter(Task.status == 'Uploaded').all()
+
+        # Imprime los resultados
+        for task in tasks:
+            print(task.id, task.status)
+
+        # Cierra la sesión
+        session.close()
 
     except Exception as e:
         raise self.retry(exc=e)
