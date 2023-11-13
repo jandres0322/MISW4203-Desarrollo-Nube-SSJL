29e3e1e7222bbcde91a64d7c4e19ebbd2ad07daf
Jhon Andres Parra Rodriguez <ja.parrar12@uniandes.edu.co>
2023-11-12 20:09:12 -0500

    Set bash to use cloud storage

--- celery/batch/convert_video.sh
+++ celery/batch/convert_video.sh
@@ -2,4 +2,8 @@
 echo "Entrando al batch"
 path_file=$1
 new_path_file=$2
+gsutil cp gs://bucketfileserver/$path_file $path_file
 ffmpeg -i $path_file -c:v libx264 -c:a aac $new_path_file
+gsutil cp $new_path_file gs://bucketfileserver/$new_path_file
+rm $path_file
+echo "Proceso completado"
